7fe962d93890070c83cf589e0874d16375de910a
xu <2190228095@qq.com>
2024-11-21 16:42:43 +0800

    changes:添加路径配置，@代表src目录，例：import HelloWorld from '@conponents/HelloWorld.vue'

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path' // 引入 path 模块
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 配置 @ 指向 src 目录
+    },
+  },
   build: {
     chunkSizeWarningLimit:1500,
     rollupOptions: {
