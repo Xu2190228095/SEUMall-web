103a4129a092c45709857413f2d3ee6abc9bab9d
Fox <614690785@qq.com>
2024-12-04 22:55:45 +0800

    try添加商品侧边栏

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
+import Sidebar from '@/views/admin/product/test_side_bar.vue'
+import Layout from '@/views/admin/layout/layout.vue'
 import HelloWorld from '@/components/HelloWorld.vue'  //引入根目录下的Hello.vue组件
 
 const router = new createRouter({
@@ -8,7 +10,7 @@ const router = new createRouter({
     {
       path: '/',     //根路径
       name: 'Hello',  //路由名称
-      component: HelloWorld  //路由组件
+      component: HelloWorld,  //路由组件
     },
     {
         path: '/home',
@@ -21,14 +23,27 @@ const router = new createRouter({
         component: () => import('@/views/User/login.vue')
     },
       {
-          path: '/admin/product',
+          path: '/product',
           name: 'Product',
-          component: () => import('@/views/admin/product/index.vue')
+          component: () => import('@/views/admin/product/index.vue'),
+          meta: { layout: Layout },
       },
       {
           path: '/admin',
           name: 'Admin',
           component: () => import('@/views/admin/index.vue')
+      },
+      {
+          path: '/test',
+          name: 'Test',
+          component: Layout,
+          meta: {title: '商品', icon: 'product'},
+          children: [{
+              path: 'product',
+              name: 'product',
+              component: () => import('@/views/admin/product/index.vue'),
+          }
+          ]
       },
     {
         path: '/productInterface',
@@ -41,7 +56,20 @@ const router = new createRouter({
       component: () => import('@/views/User/userInfo.vue')//个人信息界面
     },
   ]
-})   //创建Router实例                                     
+})   //创建Router实例
+
+export const asyncRouterMap = [
+    {
+        path: '/pms',
+        component: Layout,
+        children: [{
+            path: '/product',
+            name: 'product',
+            component: () => import('@/views/admin/product/index.vue'),
+        }
+        ]
+    },
+]
 
 export default router   //导出Router实例
 
