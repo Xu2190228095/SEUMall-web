1c4ff91edefef8059a8d4a0ad6a034fad95db47e
Fox <614690785@qq.com>
2024-12-12 01:27:43 +0800

    hello.vue显示user表信息测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,11 @@
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="deptno" label="deptno" width="180"></el-table-column>
-    <el-table-column prop="dname" label="dname" width="180"></el-table-column>
-    <el-table-column prop="loc" label="loc"></el-table-column>
+<!--    <el-table-column prop="deptno" label="deptno" width="180"></el-table-column>-->
+<!--    <el-table-column prop="dname" label="dname" width="180"></el-table-column>-->
+<!--    <el-table-column prop="loc" label="loc"></el-table-column>-->
+    <el-table-column prop="id" label="id" width="180"></el-table-column>
+    <el-table-column prop="username" label="username" width="180"></el-table-column>
+    <el-table-column prop="password" label="password"></el-table-column>
   </el-table>
 </template>
 
@@ -19,8 +22,11 @@ export default {
 
     onMounted(async () => {
       try {
-        const response = await $axios.get('/dept/list')
+        // const response = await $axios.get('/dept/list')
+        const response = await $axios.get('/user/list')
         tableData.value = response.data
+        // 在这里打印数据
+        console.log('Fetched User Data:', tableData.value);
       } catch (error) {
         console.error('Failed to fetch data:', error)
       }
